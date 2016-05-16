2a2358b0606409d1e65a739e662b4b8944823c22
AndresFelipeRinconGamboa <andres.rincon@mail.mcgill.ca>
2016-05-16 10:53:37 -0400

    Adding Poker.py
    Tricky winning solution for Poker Nim.

---
+++ PokerNim.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+def nim_sum(stacks):
+    res = 0
+
+    count = 0
+    for i in stacks:
+        res = res ^i
+ 
+    return res
+
+
+
+for x in range(n):
+    
+    in_d = int(input().split()[0])
+    count = in_d
+    current_player = 0
+    lst = input().split()
+    counter = 0
+    stacks = []
+    for j in lst:
+        stacks.append(int(j))
+  
+    if nim_sum(stacks) == 0:
+        print ("Second")
+
+    if nim_sum(stacks) > 0: 
+        print("First")
