52bfcb57de99bef1b598ffbcbbfe4406d3cd7c48
Andres Felipe Rincon Gamboa <andres.rincon@mail.mcgill.ca>
2017-08-14 23:06:57 -0400

    Create lonelyinteger.cpp

---
+++ lonelyinteger.cpp
@@ -0,0 +1,49 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int lonely_integer(vector < int > a) {
+    vector<int> sol(101);
+    for(int i = 0; i < a.size(); i++){
+        sol[a[i]] = sol[a[i]] +1;
+    }
+    for(int i = 0; i < sol.size(); i++){
+        if(sol[i] == 1){
+            return i;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[a_i];
+    }
+    cout << lonely_integer(a) << endl;
+    return 0;
+}
+
